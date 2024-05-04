faa21ced1590bcbba0d019de5d2c987191b1ec89
pawann-2000 <github.3ljwu@ggmaill.aleeas.com>
2024-05-04 23:56:42 +0530

    done

--- src/main/java/com/driver/services/impl/AdminServiceImpl.java
+++ src/main/java/com/driver/services/impl/AdminServiceImpl.java
@@ -1,11 +1,17 @@
 package com.driver.services.impl;
 
+import com.driver.model.Admin;
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
 import com.driver.repository.AdminRepository;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.services.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminServiceImpl implements AdminService {
@@ -20,13 +26,46 @@ public class AdminServiceImpl implements AdminService {
 
     @Override
     public Admin register(String username, String password) {
+        Admin admin=new Admin();
+        admin.setPassword(password);
+        admin.setUsername(username);
+        adminRepository1.save(admin);
+        return admin;
     }
 
     @Override
     public Admin addServiceProvider(int adminId, String providerName) {
+
+        ServiceProvider newServiceProvider=new ServiceProvider();
+        Admin admin=adminRepository1.findById(adminId).get();
+        newServiceProvider.setAdmin(admin);
+        newServiceProvider.setName(providerName);
+        //serviceProviderRepository1.save(newServiceProvider);
+        //get curr list & add in that
+        List<ServiceProvider> serviceProviderList=admin.getServiceProviders();
+        serviceProviderList.add(newServiceProvider);
+        admin.setServiceProviders(serviceProviderList);
+        adminRepository1.save(admin);
+        return admin;
     }
 
     @Override
     public ServiceProvider addCountry(int serviceProviderId, String countryName) throws Exception{
+        //return null;
+        countryName=countryName.toUpperCase();
+        try{
+            CountryName.valueOf(countryName);
+        }
+        catch (Exception e){
+            throw new Exception("Country not found");
+        }
+        ServiceProvider serviceProvider=serviceProviderRepository1.findById(serviceProviderId).get();
+        Country country=new Country();
+        country.setCountryName(CountryName.valueOf(countryName));
+        country.setCode(CountryName.valueOf(countryName).toCode());
+        country.setServiceProvider(serviceProvider);
+        serviceProvider.getCountryList().add(country);
+        serviceProviderRepository1.save(serviceProvider);
+        return serviceProvider;
     }
 }
--- src/main/java/com/driver/services/impl/UserServiceImpl.java
+++ src/main/java/com/driver/services/impl/UserServiceImpl.java
@@ -1,11 +1,17 @@
 package com.driver.services.impl;
 
+import com.driver.model.Country;
+import com.driver.model.CountryName;
+import com.driver.model.ServiceProvider;
+import com.driver.model.User;
 import com.driver.repository.CountryRepository;
 import com.driver.repository.ServiceProviderRepository;
 import com.driver.repository.UserRepository;
 import com.driver.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -20,10 +26,46 @@ public class UserServiceImpl implements UserService {
     @Override
     public User register(String username, String password, String countryName) throws Exception{
 
+        countryName=countryName.toUpperCase();
+        try{
+            CountryName.valueOf(countryName);
+        }
+        catch (Exception e){
+            throw new Exception("Country not found");
+        }
+        Country country = new Country();
+        country.setCountryName(CountryName.valueOf(countryName.toString()));
+        country.setCode(CountryName.valueOf(countryName).toCode());
+
+        User user=new User();
+        user.setPassword(password);
+        user.setUsername(username);
+        user.setConnected(false);
+
+        country.setUser(user);
+        user.setOriginalCountry(country);
+        user=userRepository3.save(user);
+
+//        String countryCode= CountryName.valueOf(countryName).toCode();
+//        System.out.println("Helllo "+countryCode);
+
+        //user = userRepository3.save(user);
+
+        user.setOriginalIp(new String(user.getOriginalCountry().getCode() + "." + user.getId()));
+
+        user = userRepository3.save(user);
+        return user;
     }
 
     @Override
     public User subscribe(Integer userId, Integer serviceProviderId) {
 
+        ServiceProvider serviceProvider=serviceProviderRepository3.findById(serviceProviderId).get();
+        User user=userRepository3.findById(userId).get();
+        user.getServiceProviderList().add(serviceProvider);
+        serviceProvider.getUsers().add(user);
+        serviceProviderRepository3.save(serviceProvider);
+
+        return user;
     }
 }
